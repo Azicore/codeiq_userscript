6f1b73aadc37d15bc4283db2d8fe31cce71cb241
Azicore <Azicore@users.noreply.github.com>
2018-03-02 00:20:32 +0900

    Released initial version of codeiq_badge_question_filter and codeiq_bookmark_sorter (2014/07/24)

---
+++ codeiq_badge_question_filter.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name        CodeIQ badge question filter
+// @namespace   jp.ne.sakura.azisava
+// @description Change background color of badge questions in CodeIQ top page
+// @include     https://codeiq.jp/
+// @include     https://codeiq.jp/#*
+// @version     0.1.0
+// ==/UserScript==
+
+var main = function($) {
+	var msg = '[CodeIQ badge question filter] ';
+	var css = { background: '#fc6' }; // バッジ問題に設定する背景色
+	var key = 'codeiq_badge_questions'; // localStorageのキー
+	var cache;
+	// localStorageが使えない環境では実行しない
+	try {
+		cache = (localStorage.getItem(key) || '').split(',');
+	} catch (e) {
+		console.log(msg + 'Error: localStorage is not available.');
+		return false;
+	}
+	// 現在のキャッシュデータをハッシュに入れる
+	var hash = {};
+	for (var i = 0; cache.length > i; i++) {
+		var c = cache[i].split('=');
+		hash[c[0]] = c[1];
+	}
+	cache = [];
+	var $q = $('div.cassette, div.alink');
+	var rnum = $q.size();
+	var finish = function() {
+		// 全問題を確認し終わったときの処理
+		if (!--rnum) {
+			// 新たなキャッシュデータをlocalStorageに保存する
+			localStorage.setItem(key, cache.join(','));
+			console.log(msg + 'Check completed.');
+		}
+	};
+	// 掲載中の全ての問題についてくり返す
+	$q.each(function() {
+		var $t = $(this);
+		var s = $t.hasClass('cassette');
+		var title = $(s ? 'h2' : 'h5', $t).text();
+		var $a = s ? $t : $t.prev();
+		// ウチに来ない？問題はスキップする
+		if ($('img[src="/simg/icon/101.png"]', $a).size()) {
+			console.log(msg + 'Check skipped: "' + title + '"');
+			finish();
+			return true;
+		}
+		var url = $('a', $t).last().attr('href');
+		var qnum = url.split('/').pop();
+		// キャッシュに存在する問題の場合、キャッシュのデータに従う
+		if (hash[qnum]) {
+			var b = hash[qnum] == 'y'; // バッジ問題かどうか
+			b && $t.css(css);
+			cache.push(qnum + '=' + (b ? 'y' : 'n'));
+			console.log(msg + 'Cache hit: "' + title + '"');
+			finish();
+			return true;
+		}
+		// キャッシュに存在しない問題の場合、問題ページを確認しに行く
+		$.ajax({
+			url: url,
+			dataType: 'html',
+			title: title,
+			qnum: qnum,
+			success: function(html) {
+				html = html
+					.replace(/<img src="\/img\/tit_sub09\.gif"[^>]+>/, '<span class="badgecheck"></span>')
+					.replace(/<(img|link) ([^>]+)>/g, '')
+					.replace(/<(script|iframe)([ >])/g, '<!-- $1$2')
+					.replace(/<\/(script|iframe)>/g, '</$1 -->');
+				var $c = $('span.badgecheck', html);
+				var b = $c.size() && $c.parent().next().text().indexOf('バッジ') >= 0; // バッジ問題かどうか
+				b && $t.css(css);
+				cache.push(this.qnum + '=' + (b ? 'y' : 'n'));
+				console.log(msg + 'Check completed: "' + this.title + '"');
+			},
+			error: function() {
+				console.log(msg + 'Check failed: "' + this.title + '"');
+			},
+			complete: finish
+		});
+	});
+};
+
+window.onload = function() {
+	var el = document.createElement('script');
+	el.textContent = '(' + main + ')(jQuery);';
+	document.body.appendChild(el);
+};
---
+++ codeiq_bookmark_sorter.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        CodeIQ bookmark sorting script
+// @namespace   jp.ne.sakura.azisava
+// @description Sort bookmarked questions in CodeIQ my page
+// @include     https://codeiq.jp/my_challenge_before.php
+// @include     https://codeiq.jp/my_challenge_before.php#*
+// @version     0.1.0
+// ==/UserScript==
+
+var main = function($) {
+	var msg = '[CodeIQ bookmark sorting script] ';
+	var e = [];
+	$('div.timelineBlock02').each(function() {
+		var t = $(this);
+		e.push([
+			t,
+			$('p.cdata_Limit', t).text().replace(/^\D+(\d+)\D(\d+)\D+([AP]M)(\d+)\D(\d+)\D.+$/, function(str, $1, $2, $3, $4, $5) {
+				return (+$2 + $1 * 100 + 10000) + '' + (+$5 + $4 % 12 * 100 + ($3 == 'AM' ? 10000 : 20000));
+			}),
+			$('h2', t).text()
+		]);
+	}).remove();
+	e.sort(function(a, b) {
+		return a[1] > b[1] ? 1 : -1;
+	});
+	var p = $('div.mypageTimeline');
+	for (var i = 0; e.length > i; i++) {
+		p.append(e[i][0]);
+		console.log(msg + 'Key=' + e[i][1] + '; Name="' + e[i][2] + '";');
+	}
+	console.log(msg + 'Sort completed.');
+};
+
+window.onload = function() {
+	var el = document.createElement('script');
+	el.textContent = '(' + main + ')(jQuery);';
+	document.body.appendChild(el);
+};
