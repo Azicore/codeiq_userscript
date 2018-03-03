0e44bceca92ab7e9162b2d1f9c269a5d9f08d4d2
Azicore <Azicore@users.noreply.github.com>
2018-03-04 01:58:13 +0900

    Fixed a bug of codeiq_bookmark_sorter and released ver.0.1.2 (2014/12/09)

--- codeiq_bookmark_sorter.user.js
+++ codeiq_bookmark_sorter.user.js
@@ -5,18 +5,21 @@
 // @include     https://codeiq.jp/my_challenge_before.php
 // @include     https://codeiq.jp/my_challenge_before.php#*
 // @run-at      document-end
-// @version     0.1.1
+// @version     0.1.2
 // ==/UserScript==
 
 var main = function($) {
 	var msg = '[CodeIQ bookmark sorting script] ';
 	var e = [];
+	var mon = new Date().getMonth() + 1;
 	$('div.timelineBlock02').each(function() {
 		var t = $(this);
 		e.push([
 			t,
-			$('p.cdata_Limit', t).text().replace(/^\D+(\d+)\D(\d+)\D+([AP]M)(\d+)\D(\d+)\D.+$/, function(str, $1, $2, $3, $4, $5) {
-				return (+$2 + $1 * 100 + 10000) + '' + (+$5 + $4 % 12 * 100 + ($3 == 'AM' ? 10000 : 20000));
+			$('p.cdata_Limit', t).text().replace(/([AP]M)(\d+)/, function(str, $1, $2) {
+				return $1 == 'AM' ? $2 : +$2 + 12;
+			}).replace(/^\D+(\d+)\D(\d+)\D+(\d+)\D(\d+)\D.+$/, function(str, $1, $2, $3, $4) {
+				return [+$2 + $1 * 100 + (mon > +$1 ? 20000 : 10000)] + [+$4 + $3 * 100];
 			}),
 			$('h2', t).text()
 		]);
