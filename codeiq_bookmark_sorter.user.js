4ea0735f2e5a4fb382646f1cebb77e79f0f22e92
Azicore <Azicore@users.noreply.github.com>
2018-03-06 00:27:33 +0900

    Fixed a bug of codeiq_bookmark_sorter and released ver.0.1.3 (2015/02/13)

--- codeiq_bookmark_sorter.user.js
+++ codeiq_bookmark_sorter.user.js
@@ -5,7 +5,7 @@
 // @include     https://codeiq.jp/my_challenge_before.php
 // @include     https://codeiq.jp/my_challenge_before.php#*
 // @run-at      document-end
-// @version     0.1.2
+// @version     0.1.3
 // ==/UserScript==
 
 var main = function($) {
@@ -17,9 +17,9 @@ var main = function($) {
 		e.push([
 			t,
 			$('p.cdata_Limit', t).text().replace(/([AP]M)(\d+)/, function(str, $1, $2) {
-				return $1 == 'AM' ? $2 : +$2 + 12;
+				return $1 == 'PM' && 12 > +$2 ? +$2 + 12 : $2;
 			}).replace(/^\D+(\d+)\D(\d+)\D+(\d+)\D(\d+)\D.+$/, function(str, $1, $2, $3, $4) {
-				return [+$2 + $1 * 100 + (mon > +$1 ? 20000 : 10000)] + [+$4 + $3 * 100];
+				return (mon > +$1 ? 2 : 1) * 1e8 + $1 * 1e6 + $2 * 1e4 + $3 * 100 + $4 * 1;
 			}),
 			$('h2', t).text()
 		]);
