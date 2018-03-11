dc75e77b5402d8c8b8be4868ece3145be1439d1e
Azicore <Azicore@users.noreply.github.com>
2018-03-12 00:07:38 +0900

    Improved codeiq_badge_question_filter and released ver.0.2.1 (2016/01/07)

--- codeiq_badge_question_filter.user.js
+++ codeiq_badge_question_filter.user.js
@@ -6,13 +6,17 @@
 // @include     https://codeiq.jp/#*
 // @include     https://codeiq.jp/q/search*
 // @run-at      document-end
-// @version     0.2.0
+// @version     0.2.1
 // ==/UserScript==
 
 var main = function($) {
 	var msg = '[CodeIQ badge question filter] ';
-	var css = { background: '#fc6' }; // バッジ問題に設定する背景色
+	var css = [ // バッジ問題に設定する背景色
+		{ background: '#f30' }, // 未挑戦、かつ、あとで挑戦に未登録
+		{ background: '#fc6' }  // 挑戦済み、または、あとで挑戦に登録済み
+	];
 	var key = 'codeiq_badge_questions'; // localStorageのキー
+	var maxlog = 200; // キャッシュする問題数の上限
 	var cache;
 	// localStorageが使えない環境では実行しない
 	try {
@@ -33,6 +37,9 @@ var main = function($) {
 	var finish = function() {
 		// 全問題を確認し終わったときの処理
 		if (!--rnum) {
+			for (var i in hash) cache.push(i + '=' + hash[i]);
+			// 古いキャッシュを消去する
+			cache.length = Math.min(cache.length, maxlog);
 			// 新たなキャッシュデータをlocalStorageに保存する
 			localStorage.setItem(key, cache.join(','));
 			console.log(msg + 'Check completed.');
@@ -42,14 +49,26 @@ var main = function($) {
 	$q.each(function() {
 		var $t = $(this);
 		var $h = $('h3', $t);
+		var confirmed = $('.unflag-action', $t).length > 0 || $('img[alt="挑戦済み"]', $t).length > 0;
 		var title = $h.text();
 		var url = $('a', $h).attr('href');
 		var qnum = url.split('/').pop();
+		// 各問題に対する処理
+		var check = function(isBadge) {
+			if (isBadge) {
+				$t.css(css[+confirmed]);
+				$t.on('click', '.flag', function() {
+					confirmed = !confirmed;
+					$t.css(css[+confirmed]);
+				});
+			}
+			cache.push(qnum + '=' + (isBadge ? 'y' : 'n'));
+			delete hash[qnum];
+		};
 		// キャッシュに存在する問題の場合、キャッシュのデータに従う
 		if (hash[qnum]) {
-			var b = hash[qnum] == 'y'; // バッジ問題かどうか
-			b && $t.css(css);
-			cache.push(qnum + '=' + (b ? 'y' : 'n'));
+			var isBadge = hash[qnum] == 'y'; // バッジ問題かどうか
+			check(isBadge);
 			console.log(msg + 'Cache hit: "' + title + '"');
 			finish();
 			return true;
@@ -58,8 +77,6 @@ var main = function($) {
 		$.ajax({
 			url: url,
 			dataType: 'html',
-			title: title,
-			qnum: qnum,
 			success: function(html) {
 				html = html
 					.replace(/<img src="[^"]*tit_sub09\.gif"[^>]+>/, '<span class="badgecheck"></span>')
@@ -67,13 +84,12 @@ var main = function($) {
 					.replace(/<(script|iframe)([ >])/g, '<!-- $1$2')
 					.replace(/<\/(script|iframe)>/g, '</$1 -->');
 				var $c = $('span.badgecheck', html);
-				var b = $c.size() && $c.parent().next().text().indexOf('バッジ') >= 0; // バッジ問題かどうか
-				b && $t.css(css);
-				cache.push(this.qnum + '=' + (b ? 'y' : 'n'));
-				console.log(msg + 'Check completed: "' + this.title + '"');
+				var isBadge = $c.size() && $c.parent().next().text().indexOf('バッジ') >= 0; // バッジ問題かどうか
+				check(isBadge);
+				console.log(msg + 'Check success: "' + title + '"');
 			},
 			error: function() {
-				console.log(msg + 'Check failed: "' + this.title + '"');
+				console.log(msg + 'Check failed: "' + title + '"');
 			},
 			complete: finish
 		});
