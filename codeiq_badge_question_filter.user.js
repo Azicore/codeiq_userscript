464b6390db668a4bb8606897e6938a2b596780a1
Azicore <Azicore@users.noreply.github.com>
2018-03-15 00:31:55 +0900

    Fixed a bug of codeiq_badge_question_filter and released ver.0.2.2 (2016/09/08)

--- codeiq_badge_question_filter.user.js
+++ codeiq_badge_question_filter.user.js
@@ -6,21 +6,19 @@
 // @include     https://codeiq.jp/#*
 // @include     https://codeiq.jp/q/search*
 // @run-at      document-end
-// @version     0.2.1
+// @version     0.2.2
 // ==/UserScript==
 
 var main = function($) {
 	var msg = '[CodeIQ badge question filter] ';
-	var css = [ // バッジ問題に設定する背景色
-		{ background: '#f30' }, // 未挑戦、かつ、あとで挑戦に未登録
-		{ background: '#fc6' }  // 挑戦済み、または、あとで挑戦に登録済み
-	];
+	var css = { background: '#fc6' }; // バッジ問題に設定する背景色
 	var key = 'codeiq_badge_questions'; // localStorageのキー
 	var maxlog = 200; // キャッシュする問題数の上限
 	var cache;
 	// localStorageが使えない環境では実行しない
 	try {
-		cache = (localStorage.getItem(key) || '').split(',');
+		var lsval = localStorage.getItem(key);
+		cache = lsval ? lsval.split(',') : [];
 	} catch (e) {
 		console.log(msg + 'Error: localStorage is not available.');
 		return false;
@@ -32,12 +30,12 @@ var main = function($) {
 		hash[c[0]] = c[1];
 	}
 	cache = [];
-	var $q = $('.topListBox');
-	var rnum = $q.size();
+	var $q = $('a.panel-codeiq, a.pickupBox[href*="site_type=0"], a.panel-item[href*="site_type=0"]');
+	var qnum = $q.length;
 	var finish = function() {
 		// 全問題を確認し終わったときの処理
-		if (!--rnum) {
-			for (var i in hash) cache.push(i + '=' + hash[i]);
+		if (!--qnum) {
+			for (var i in hash) if (i !== '') cache.push(i + '=' + hash[i]);
 			// 古いキャッシュを消去する
 			cache.length = Math.min(cache.length, maxlog);
 			// 新たなキャッシュデータをlocalStorageに保存する
@@ -46,28 +44,26 @@ var main = function($) {
 		}
 	};
 	// 掲載中の全ての問題についてくり返す
+	var qlist = {};
 	$q.each(function() {
 		var $t = $(this);
-		var $h = $('h3', $t);
-		var confirmed = $('.unflag-action', $t).length > 0 || $('img[alt="挑戦済み"]', $t).length > 0;
-		var title = $h.text();
-		var url = $('a', $h).attr('href');
-		var qnum = url.split('/').pop();
+		var qid = $t.attr('href').match(/\d+$/)[0];
+		var $e = qlist[qid];
+		qlist[qid] = $e ? $e.add($t) : $t;
+	});
+	$.each(qlist, function(qid, $t) {
+		var $h = $t.find('.panel-title, .pickupBox-title, .item-title').eq(0);
+		var title = $h.text().replace(/^[ \n]+|[ \n]+$/g, '');
+		var url = '/q/' + qid;
 		// 各問題に対する処理
 		var check = function(isBadge) {
-			if (isBadge) {
-				$t.css(css[+confirmed]);
-				$t.on('click', '.flag', function() {
-					confirmed = !confirmed;
-					$t.css(css[+confirmed]);
-				});
-			}
-			cache.push(qnum + '=' + (isBadge ? 'y' : 'n'));
-			delete hash[qnum];
+			if (isBadge) $t.css(css);
+			cache.push(qid + '=' + (isBadge ? 'y' : 'n'));
+			delete hash[qid];
 		};
 		// キャッシュに存在する問題の場合、キャッシュのデータに従う
-		if (hash[qnum]) {
-			var isBadge = hash[qnum] == 'y'; // バッジ問題かどうか
+		if (hash[qid]) {
+			var isBadge = hash[qid] == 'y'; // バッジ問題かどうか
 			check(isBadge);
 			console.log(msg + 'Cache hit: "' + title + '"');
 			finish();
@@ -84,7 +80,7 @@ var main = function($) {
 					.replace(/<(script|iframe)([ >])/g, '<!-- $1$2')
 					.replace(/<\/(script|iframe)>/g, '</$1 -->');
 				var $c = $('span.badgecheck', html);
-				var isBadge = $c.size() && $c.parent().next().text().indexOf('バッジ') >= 0; // バッジ問題かどうか
+				var isBadge = $c.length && $c.parent().next().text().indexOf('バッジ') >= 0; // バッジ問題かどうか
 				check(isBadge);
 				console.log(msg + 'Check success: "' + title + '"');
 			},
